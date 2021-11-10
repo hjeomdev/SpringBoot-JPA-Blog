93fa5120c0d7c463245a6781aca4ebb45cabbac6
Hyejin Eom <yazbyz@gmail.com>
2021-11-10 15:33:56 +0900

    [UPDATE] lombok: @Getter. @Setter, @Data, @AllArgsStructor, @NoArgsStructor, @Builder)

--- src/main/java/com/yazbyz/blog/test/HttpControllerTest.java
+++ src/main/java/com/yazbyz/blog/test/HttpControllerTest.java
@@ -12,11 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // 사용자의 요청에 데이터로 응답하는 컨트롤러 
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest:";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@naver.com").build();
+		System.out.println(TAG + "getter: " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG + "setter: " + m.getUsername());
+		return "lombok test 완료";
+	}
+	
 	// 인터넷 브라우저 요청은 무조건 get요청 밖에 할 수 없다.
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getText(Member m) { // 쿼리 스트링 한번에 받기(자동으로 매핑되어 객체에 들어온다) 
-			//@RequestParam int id, @RequestParam String username) { // 쿼리 스트링 하나하나 받기 
+			//@RequestParam int id, @RequestParam String username) { // 쿼리 스트링 하나하나 받기
 		return "get 요청" + m.getId() + m.getUsername() + m.getPassword() + m.getEmail();
 	}
 	
