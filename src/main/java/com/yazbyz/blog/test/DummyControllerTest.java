bb7af3426cacfdf808209d7de8abb1aab72f2992
Hyejin Eom <yazbyz@gmail.com>
2021-11-19 00:56:44 +0900

    [UPDATE] JPA delete

--- src/main/java/com/yazbyz/blog/test/DummyControllerTest.java
+++ src/main/java/com/yazbyz/blog/test/DummyControllerTest.java
@@ -6,9 +6,11 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +28,17 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입 
 	private UserRepository userRepository;
 	
+	@DeleteMapping("dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		
+		return "삭제되었습니다. id: " + id;
+	}
+	
 	//http://localhost:8000/blog/dummy/user/1 (요청)
 	// email, password 수정 
 	@Transactional
@@ -38,7 +51,9 @@ public class DummyControllerTest {
 		user.setPassword(requestUser.getPassword());
 		
 //		userRepository.save(user);
-		return null;
+		
+		// 더티체킹 
+		return user;
 	}
 		
 	//http://localhost:8000/blog/dummy/users (요청)
