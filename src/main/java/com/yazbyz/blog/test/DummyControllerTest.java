ac5b3304eac0cb5e1ffa60af3bbcecd4f3be9a54
Hyejin Eom <yazbyz@gmail.com>
2021-11-18 23:33:19 +0900

    [UPDATE] JPA select (get, orElseGet, orElseThrow, IllegalArgumentException, 람다식, MessageConverter)

--- src/main/java/com/yazbyz/blog/test/DummyControllerTest.java
+++ src/main/java/com/yazbyz/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.yazbyz.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,38 @@ import com.yazbyz.blog.model.RoleType;
 import com.yazbyz.blog.model.User;
 import com.yazbyz.blog.repository.UserRepository;
 
-@RestController
+@RestController // html파일이 아니라 data를 리턴해주는 controller
 public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입 
 	private UserRepository userRepository;
+	
+	//http://localhost:8000/blog/dummy/user/1 (요청)
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) { 
+//		User users = userRepository.findById(id).get(); // null 일 경우 에러 
+//		User users = userRepository.findById(id).orElseGet(new Supplier<User>() { // 추천 안 함 
+//			@Override
+//			public User get() {
+//				return new User(); // 빈 객체 반환 
+//			}
+//		});
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id); // 이 부분으로 에러페이지 사용 가능 
+			}
+		});
+//		User user = userRepository.findById(id).orElseThrow(() -> { // 위 표현의 람다식 표현 (편리하다)
+//			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+//		});
+		
+		// 요청: 웹브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환(웹브라우저가 이해할 수 있는 데이터) -> json
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동하여 객체를 json으로 변환하여 반환 
+		return user;
+	}
 	
 	//http://localhost:8000/blog/dummy/join (요청)
 	//http의 body에 username, password, email 데이터를 가지고 (요청)  
