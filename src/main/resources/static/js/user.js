7910f38e6c35281fdfb3f5c48ac8b4836f54fbb6
Hyejin Eom <yazbyz@gmail.com>
2021-11-27 01:23:59 +0900

    [UPDATE] 주소 변경

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -23,13 +23,13 @@ let index = {
 		$.ajax({
 			// 회원가입 수행 요청 
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/api/user",
 			data: JSON.stringify(data), // http body데이터 
 			contentType: "application/json; charset=utf-8", // body 데이터 타입(MIME)
 			dataType: "json" // 요청이 서버로해서 응답이 왔을 때 기본적으로 모든것이 문자열(생긴게 json이라면) => js오브젝트로 변경 
 		}).done(function(resp){
 			alert("회원가입이 완료");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});  
@@ -44,13 +44,13 @@ let index = {
 		$.ajax({
 			// 회원가입 수행 요청 
 			type: "POST",
-			url: "/blog/api/user/login",
+			url: "/api/user/login",
 			data: JSON.stringify(data), // http body데이터 
 			contentType: "application/json; charset=utf-8", // body 데이터 타입(MIME)
 			dataType: "json" // 요청이 서버로해서 응답이 왔을 때 기본적으로 모든것이 문자열(생긴게 json이라면) => js오브젝트로 변경 
 		}).done(function(resp){
 			alert("로그인이 완료");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});  
