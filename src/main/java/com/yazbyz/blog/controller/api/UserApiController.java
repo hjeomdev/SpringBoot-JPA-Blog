afeaec82a1ceb1d8020f7420545d261408ba62fc
Hyejin Eom <yazbyz@gmail.com>
2021-11-27 01:16:44 +0900

    [UPDATE] HttpSession을 IoC로 관리

--- src/main/java/com/yazbyz/blog/controller/api/UserApiController.java
+++ src/main/java/com/yazbyz/blog/controller/api/UserApiController.java
@@ -19,6 +19,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession httpSession;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출됨.");
@@ -29,7 +32,7 @@ public class UserApiController {
 	}
 	
 	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+	public ResponseDto<Integer> login(@RequestBody User user) {
 		System.out.println("UserApiController: login 호출됨.");
 		User principal = userService.로그인(user); //principal == 접근주체 
 		
